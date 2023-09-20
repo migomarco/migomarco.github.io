a0d44c997fe600d13ea6ec9347691373d8599958
migomarco <migomarco1@hotmail.com>
2023-09-20 09:50:32 +0200

    Pass Diriyah Update

--- js/index.js
+++ js/index.js
@@ -23,3 +23,43 @@
     } // End if
   });
 });
+
+
+$(document).ready(function() {
+    // Store the correct password
+    const correctPassword = "123";
+
+    // Show the overlay initially
+    $("#overlay").removeClass("hidden");
+
+    // Function to handle the password submission
+    $("#submitButton").click(function() {
+        const enteredPassword = $("#passwordInput").val();
+        if (enteredPassword === correctPassword) {
+            // Hide the overlay and remove blur on content
+            
+            $("#overlay").css("visibility", "hidden");
+            $("#Blur").css("filter", "blur(0px)");
+            
+
+        } else {
+            alert("Incorrect password. Please try again.");
+        }
+    });
+
+    // Function to access blurred version (no password required)
+    $("#accessBlurredVersion").click(function(event) {
+        event.preventDefault();
+        // Add blur on content
+        $("#overlay").css("visibility", "hidden");
+       
+    });
+
+    // Add event listener for Enter key press
+    $("#passwordInput").keypress(function(event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            $("#submitButton").click(); // Trigger click on #submitButton
+        }
+    });
+});
