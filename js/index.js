01b8b46904becb2fd89066ce3e932dbe95fafc27
migomarco <migomarco1@hotmail.com>
2023-09-20 14:06:53 +0200

    JS adjust page then href

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 
+// the code to jump to window viewer 
+
   $(document).ready(function(){
   // Add smooth scrolling to all links
   $("a").on('click', function(event) {
@@ -19,47 +21,83 @@
    
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
+          });
+        } // End if
       });
-    } // End if
-  });
-});
-
-
-$(document).ready(function() {
-    // Store the correct password
-    const correctPassword = "123";
-
-    // Show the overlay initially
-    $("#overlay").removeClass("hidden");
-
-    // Function to handle the password submission
-    $("#submitButton").click(function() {
-        const enteredPassword = $("#passwordInput").val();
-        if (enteredPassword === correctPassword) {
-            // Hide the overlay and remove blur on content
-            
-            $("#overlay").css("visibility", "hidden");
-            $("#Blur").css("filter", "blur(0px)");
-            
-
-        } else {
-            alert("Incorrect password. Please try again.");
-        }
     });
 
-    // Function to access blurred version (no password required)
-    $("#accessBlurredVersion").click(function(event) {
-        event.preventDefault();
-        // Add blur on content
-        $("#overlay").css("visibility", "hidden");
-       
-    });
 
-    // Add event listener for Enter key press
-    $("#passwordInput").keypress(function(event) {
-        if (event.key === "Enter") {
+// the code to readjust viewer then open url 
+
+    $(document).ready(function() {
+        // Select all the links with class names 
+        const links = $(".Diriyah, .Modelo, .Volvo, .AntiAsian, .rest, .emirates, .johnjohnlab, .frank");
+
+        // Add a click event listener to each link
+        links.click(function(event) {
+            // Prevent the default link behavior (e.g., navigating to the href)
             event.preventDefault();
-            $("#submitButton").click(); // Trigger click on #submitButton
-        }
+
+            // Get the current href attribute
+            const currentHref = $(this).attr("href");
+
+            // Run the existing href by navigating to it
+            window.location.href = currentHref;
+
+            // Delay the navigation to the new URL to allow the existing href to execute
+            setTimeout(() => {
+                // Get the current class name
+                const currentClassName = $(this).attr("class");
+
+                // Construct the new URL based on the class name
+                const newURL = currentClassName + ".html";
+
+                // Navigate to the new URL
+                window.location.href = newURL;
+            }, 800); // Adjust the delay time (in milliseconds) if needed
+        });
     });
-});
+
+
+
+
+// Password code 
+
+    $(document).ready(function() {
+        // Store the correct password
+        const correctPassword = "123";
+
+        // Show the overlay initially
+        $("#overlay").removeClass("hidden");
+
+        // Function to handle the password submission
+        $("#submitButton").click(function() {
+            const enteredPassword = $("#passwordInput").val();
+            if (enteredPassword === correctPassword) {
+                // Hide the overlay and remove blur on content
+                
+                $("#overlay").css("visibility", "hidden");
+                $("#Blur").css("filter", "blur(0px)");
+                
+
+            } else {
+                alert("Incorrect password. Please try again.");
+            }
+        });
+
+        // Function to access blurred version (no password required)
+        $("#accessBlurredVersion").click(function(event) {
+            event.preventDefault();
+            // Add blur on content
+            $("#overlay").css("visibility", "hidden");
+           
+        });
+
+        // Add event listener for Enter key press
+        $("#passwordInput").keypress(function(event) {
+            if (event.key === "Enter") {
+                event.preventDefault();
+                $("#submitButton").click(); // Trigger click on #submitButton
+            }
+        });
+    });
